c71a968d6d1f98c97466053273f68033180cf453
HighSoftWare96 <bertonc96@gmail.com>
2017-03-07 01:40:54 +0100

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Hey there!';
+  name = 'Giovanni';
+  surname = 'Bertoncelli';
+  notMatches = false;
+  errors = false;
+
+  checkPSW(value1: string, value2: string){
+    console.log("Inserted something!");
+    if(!value1.match(value2) && this.notMatches == false){
+      this.notMatches = true;
+      this.errors = true;
+    }
+    else {
+      this.notMatches = false;
+      //non ci sono problemi
+      this.errors = false;
+    }
+  }
 }
